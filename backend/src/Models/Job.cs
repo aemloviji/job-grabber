0fd28f2dbda0f7963ed8df3ddbdac9d4648f420e
AEMLoviji <AEMLoviji@gmail.com>
2020-05-03 01:58:34 +0300

    feat: enhance the list of available properties for Job model

--- backend/src/Models/Job.cs
+++ backend/src/Models/Job.cs
@@ -10,6 +10,14 @@ namespace JobGrabber.Backend.Models
 
         public string Company { get; set; }
 
-        public string CompanyUrl { get; set; }
+        public string CompanyLogo { get; set; }
+
+        public string Location { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public string Url { get; set; }
     }
 }
